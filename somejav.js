510f292f58dbf43a406dc0508a4f57a1a735ca12
Rachana S. <111532996+raceshar@users.noreply.github.com>
2024-12-07 22:28:12 -0500

    Add files via upload

---
+++ somejav.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".menu");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
+});
